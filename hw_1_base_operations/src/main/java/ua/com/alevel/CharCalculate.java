a93aecd0dd1e58bc342f2e080557153045f176e1
Bohdan Abramovych <voiceofdead999@gmail.com>
2021-10-15 21:45:34 +0300

    final fix in CharCalculate

--- hw_1_base_operations/src/main/java/ua/com/alevel/CharCalculate.java
+++ hw_1_base_operations/src/main/java/ua/com/alevel/CharCalculate.java
@@ -1,40 +1,48 @@
 package ua.com.alevel;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class CharCalculate {
     public CharCalculate() {
-
         System.out.print("Печатайте: ");
         Scanner g = new Scanner(System.in);
         String h = g.nextLine();
-        char[] ch = h.toCharArray();
+        boolean isNumeric = h.chars().allMatch(Character::isDigit);
 
-        Arrays.sort(ch);
-        int len = ch.length;
-        if (len < 2) {
-            System.out.println("Введите больше одного значения!");
+        if (isNumeric) {
+            System.out.println("Вы ввели только цыфры!");
         } else {
+            char[] ch = h.toCharArray();
+            Character[] newArray = IntStream.range(0, ch.length)
+                    .mapToObj(i -> ch[i])
+                    .toArray(Character[]::new);
+            Arrays.sort(newArray);
+            ArrayList<Character> newCh = new ArrayList<>();
+            int len = newArray.length;
             int index = 0;
             int count = 0;
-            char symbol = ch[len - 1];
-            for (int i = len - 1; i >= 0; i--) {
-                if (Character.isLetter(ch[i])) {
-                    if (ch[i] == symbol) {
-                        count++;
-                    } else {
-                        System.out.println(++index + ". " + symbol + ": " + count);
-                        count = 1;
-                        symbol = ch[i];
-                    }
+
+            IntStream.range(0, len).filter(i -> Character.isLetter(newArray[i])).forEach(i -> {
+                newCh.add(newArray[i]);
+            });
+            int len1 = newCh.size();
+            int j = 0;
+            char symbol = newCh.get(0);
+
+            while (j < len1) {
+                if (newCh.get(j) == symbol) {
+                    count++;
+                } else {
+                    System.out.println(++index + ". " + symbol + ": " + count);
+                    count = 1;
+                    symbol = newCh.get(j);
                 }
+                j++;
             }
-            if (Character.isLetter(symbol)) {
-                System.out.println(++index + ". " + symbol + ": " + count);
-            } else {
-                System.out.println("Вы ввели только цифры!");
-            }
+            System.out.println(++index + ". " + symbol + ": " + count);
         }
     }
 }
