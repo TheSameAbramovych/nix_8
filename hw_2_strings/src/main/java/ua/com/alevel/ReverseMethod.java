ab75442c0965a501a088803de665a01737f281a8
Bohdan Abramovych <voiceofdead999@gmail.com>
2021-10-17 18:14:23 +0300

    Recreate ReverseMethod

--- hw_2_strings/src/main/java/ua/com/alevel/ReverseMethod.java
+++ hw_2_strings/src/main/java/ua/com/alevel/ReverseMethod.java
@@ -4,32 +4,19 @@ import java.util.Scanner;
 
 public class ReverseMethod {
     public static void main(String[] args) {
-        String s;
-        StringBuilder rev = new StringBuilder();
-        Scanner in = new Scanner(System.in);
+        System.out.println("Print : ");
+        Scanner g = new Scanner(System.in);
+        String str = g.nextLine();
 
-        System.out.println("Печатайте: ");
-        s = in.nextLine();
-
-        int len = s.length();
-        int l, r;
-        int i = 0;
-        while (i < len) {
-            l = i;
-            while (i < len && s.charAt(i) != ' ') {
-                i++;
+        String[] arrStr = str.split(" ");
+        for (String s : arrStr) {
+            char[] ch = s.toCharArray();
+            for (int j = ch.length - 1; j >= 0; j--) {
+                System.out.print(ch[j]);
             }
-            r = i - 1;
-            for (int j = r; j >= l; j--) {
-                rev.append(s.charAt(j));
-            }
-            rev.append(" ");
-            i++;
+            System.out.print(" ");
         }
-
-        System.out.println("Результат:  " + rev);
     }
-
 }
 
 
