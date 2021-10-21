231d2cf5940deddf61e06686753bd6fa23f00ff9
Bohdan Abramovych <voiceofdead999@gmail.com>
2021-10-21 21:59:21 +0300

    create hw_3_start_oop

---
+++ hw_3_start_oop/src/main/java/ua/com/alevel/StartOopMain.java
@@ -0,0 +1,10 @@
+package ua.com.alevel;
+
+import ua.com.alevel.view.UserController;
+
+public class StartOopMain {
+
+    public static void main(String[] args) {
+        new UserController().run();
+    }
+}
