6faddc9d7c1ba447e4ab1deade15ccde24583bd7
Bohdan Abramovych <voiceofdead999@gmail.com>
2021-11-03 21:08:20 +0200

    start module_1

--- module_1/src/main/java/ua/com/alevel/level_1/TriangleArea.java
+++ module_1/src/main/java/ua/com/alevel/level_1/TriangleArea.java
@@ -3,22 +3,20 @@ package ua.com.alevel.level_1;
 import java.util.Scanner;
 
 public class TriangleArea {
-    String str;
-    int[] array;
 
     public TriangleArea() {
-        System.out.println("Print coordinate dot A");
-        ArraysInteger();
+        System.out.println("Print coordinate dot A: ");
+        int[] array = readDot();
         int x1 = array[0];
         int y1 = array[1];
 
-        System.out.println("Print coordinate dot B");
-        ArraysInteger();
+        System.out.println("Print coordinate dot B: ");
+        array = readDot();
         int x2 = array[0];
         int y2 = array[1];
 
-        System.out.println("Print coordinate dot C : ");
-        ArraysInteger();
+        System.out.println("Print coordinate dot C: ");
+        array = readDot();
         int x3 = array[0];
         int y3 = array[1];
 
@@ -26,16 +24,17 @@ public class TriangleArea {
         System.out.println("Square triangular = " + square);
     }
 
-    public void ArraysInteger() {
+    public int[] readDot() {
         Scanner scan = new Scanner(System.in);
-        str = scan.nextLine();
+        String str = scan.nextLine();
 
         String[] arrStr = str.split(",");
-        array = new int[arrStr.length];
+        int[] array = new int[arrStr.length];
 
         for (int i = 0; i < arrStr.length; i++) {
             array[i] = Integer.parseInt(arrStr[i]);
         }
+        return array;
     }
 
 }
