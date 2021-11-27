7e61b8eac63911416008b05719c8630997e7393a
Bohdan Abramovych <voiceofdead999@gmail.com>
2021-11-27 19:15:29 +0200

    add some changes in hw

--- hw_5_collection/src/main/java/ua/com/alevel/MathSetMain.java
+++ hw_5_collection/src/main/java/ua/com/alevel/MathSetMain.java
@@ -4,12 +4,53 @@ import ua.com.alevel.util.MathSet;
 
 public class MathSetMain {
     public static void main(String[] args) {
-        Integer[] ints = {1, 2, 3};
-        Integer[] ints2 = {4, 5, 6};
-        MathSet<Integer> mathSet = new MathSet<>(1, 2);
+        MathSet<Integer> mathSet = new MathSet<>();
+        System.out.print("ADD(122, 12, 32, 11, 27, 21, 2, 3, 7, 9, 5, 12, 8, 9, 9, 12, 12, 23, 45): ");
+        mathSet.add(122, 12, 32, 11, 27, 21, 2, 3, 7, 9, 5, 12, 8, 9, 9, 12, 12, 23, 45);
+        System.out.println(mathSet);
 
-        ints[2] = 4;
+        System.out.print("MIN: ");
+        System.out.println(mathSet.getMin());
+        System.out.print("MAX: ");
+        System.out.println(mathSet.getMax());
 
-        System.out.println(mathSet.get(2));
+        System.out.print("MEDIAN: ");
+        System.out.println(mathSet.getMedian());
+
+        mathSet.sortAsc(3);
+        System.out.println("SORT_ASK(3): " + mathSet);
+
+        mathSet.sortAsc();
+        System.out.println("SORT_ASK: " + mathSet);
+
+        mathSet.sortDesc(7);
+        System.out.print("SORT_DESC(7): ");
+        System.out.println(mathSet);
+
+        mathSet.sortDesc();
+        System.out.print("SORT_DESC: ");
+        System.out.println(mathSet);
+
+        System.out.println("CUT_ARRAY: " + mathSet.cut(0, 6));
+        System.out.println("CUT(0,6): " + mathSet);
+
+        System.out.print("AVERAGE: ");
+        System.out.println(mathSet.getAverage());
+
+        System.out.print("CLEAR(12): ");
+        mathSet.clear(12);
+        System.out.println(mathSet);
+
+        System.out.print("JOIN(35, 36; 999, 666): ");
+        mathSet.join(new MathSet<>(35, 36), new MathSet<>(999, 666));
+        System.out.println(mathSet);
+
+        System.out.print("INTERSECTION(9, 8): ");
+        mathSet.intersection(new MathSet<>(9, 8));
+        System.out.println(mathSet);
+
+        mathSet.clear(999, 666);
+        System.out.print("CLEAR(999,666): " + mathSet);
+
     }
 }
--- hw_5_collection/src/main/java/ua/com/alevel/util/MathSet.java
+++ hw_5_collection/src/main/java/ua/com/alevel/util/MathSet.java
@@ -1,5 +1,6 @@
 package ua.com.alevel.util;
 
+import java.util.Objects;
 
 public class MathSet<N extends Number> {
 
@@ -14,19 +15,29 @@ public class MathSet<N extends Number> {
         numbers = createNumbersArray(capacity);
     }
 
+    @SafeVarargs
     public MathSet(N... numbers) {
         this(numbers.length);
         this.pointer = numbers.length;
         System.arraycopy(numbers, 0, this.numbers, 0, numbers.length);
     }
 
+    @SafeVarargs
     public MathSet(N[]... numbers) {
+        for (N[] number : numbers) {
+            add(number);
+        }
     }
 
-    public MathSet(MathSet numbers) {
+    public MathSet(MathSet<N> numbers) {
+        join(numbers);
     }
 
-    public MathSet(MathSet... numbers) {
+    @SafeVarargs
+    public MathSet(MathSet<N>... numbers) {
+        for (MathSet<N> number : numbers) {
+            join(number);
+        }
     }
 
     public void add(N n) {
@@ -37,50 +48,107 @@ public class MathSet<N extends Number> {
         pointer++;
     }
 
-    public void add(N... numbers) {
-        for (N n : numbers) {
-            add(n);
+    @SafeVarargs
+    public final void add(N... numbers) {
+        copyToNewArray(numbers.length + pointer);
+        System.arraycopy(numbers, 0, this.numbers, pointer, numbers.length);
+        pointer += numbers.length;
+    }
+
+    public void join(MathSet<N> ms) {
+//        Number[] numbers1 = ms.toArray();
+//
+//        int capacity = numbers1.length + pointer;
+//        N[] joinArray = createNumbersArray(capacity);
+//
+//        System.arraycopy(numbers,0, joinArray, 0, pointer);
+//        System.arraycopy(numbers1, 0, joinArray, pointer, numbers1.length);
+//        numbers = joinArray;
+//        pointer = capacity;
+
+        add((N[]) ms.toArray());
+    }
+
+    @SafeVarargs
+    public final void join(MathSet<N>... ms) {
+        for (MathSet<N> m : ms) {
+            join(m);
         }
     }
 
-    public void join(MathSet ms) {
-
+    public void intersection(MathSet<N> ms) {
+        clear((N[]) ms.toArray());
     }
 
-    public void join(MathSet... ms) {
-
-    }
-
-    public void intersection(MathSet ms) {
-
-    }
-
-    public void intersection(MathSet... ms) {
-
+    @SafeVarargs
+    public final void intersection(MathSet<N>... ms) {
+        for (MathSet<N> m : ms) {
+            intersection(ms);
+        }
     }
 
     public void sortDesc() {
-
+        sortDesc(0, pointer);
     }
 
     public void sortDesc(int firstIndex, int lastIndex) {
-
+        if (lastIndex >= pointer) {
+            lastIndex = pointer - 1;
+        }
+        for (int i = firstIndex; i <= lastIndex; i++) {
+            N max = numbers[i];
+            int minId = i;
+            for (int j = i + 1; j <= lastIndex; j++) {
+                if (max.doubleValue() < numbers[j].doubleValue()) {
+                    max = numbers[j];
+                    minId = j;
+                }
+            }
+            N temp = numbers[i];
+            numbers[i] = max;
+            numbers[minId] = temp;
+        }
     }
 
     public void sortDesc(N value) {
-
+        for (int i = 0; i < pointer; i++) {
+            if (Objects.equals(value, numbers[i])) {
+                sortDesc(i, pointer);
+                break;
+            }
+        }
     }
 
     public void sortAsc() {
-
+        sortAsc(0, pointer);
     }
 
     public void sortAsc(int firstIndex, int lastIndex) {
-
+        if (lastIndex >= pointer) {
+            lastIndex = pointer - 1;
+        }
+        for (int i = firstIndex; i <= lastIndex; i++) {
+            N min = numbers[i];
+            int minId = i;
+            for (int j = i + 1; j <= lastIndex; j++) {
+                if (min.doubleValue() > numbers[j].doubleValue()) {
+                    min = numbers[j];
+                    minId = j;
+                }
+            }
+            N temp = numbers[i];
+            numbers[i] = min;
+            numbers[minId] = temp;
+        }
     }
 
     public void sortAsc(N value) {
-
+        for (int i = 0; i < pointer; i++) {
+            if (Objects.equals(value, numbers[i])) {
+                sortAsc(i, pointer);
+                break;
+            }
+        }
     }
 
     public N get(int index) {
@@ -91,48 +159,136 @@ public class MathSet<N extends Number> {
     }
 
     public N getMax() {
-        return null;
+        if (pointer <= 0) {
+            return null;
+        }
+        N max = numbers[0];
+        for (int i = 0; i < pointer; i++) {
+            max = max.doubleValue() < numbers[i].doubleValue() ? numbers[i] : max;
+        }
+        return max;
     }
 
     public N getMin() {
-        return null;
+        if (pointer <= 0) {
+            return null;
+        }
+        N min = numbers[0];
+        for (int i = 0; i < pointer; i++) {
+            min = min.doubleValue() > numbers[i].doubleValue() ? numbers[i] : min;
+        }
+        return min;
     }
 
-    public N getAverage() {
-        return null;
+    public double getAverage() {
+        double sum = 0;
+
+        for (int i = 0; i < pointer; i++) {
+            sum += numbers[i].doubleValue();
+
+        }
+        return sum / pointer;
     }
 
-    public N getMedian() {
-        return null;
+    public double getMedian() {
+        if (pointer == 0) {
+            return 0;
+        }
+        int index = pointer / 2;
+        if (pointer % 2 == 0) {
+            return (get(index).doubleValue() + get(index - 1).doubleValue()) / 2;
+        }
+        return get(index).doubleValue();
     }
 
-    public N[] toArray() {
-        return null;
+    public Number[] toArray() {
+        return toArray(0, pointer);
     }
 
-    public N[] toArray(int firstIndex, int lastIndex) {
-        return null;
+    public Number[] toArray(int firstIndex, int lastIndex) {
+        if (firstIndex < 0 || firstIndex > lastIndex) {
+            firstIndex = 0;
+        }
+        if (lastIndex > pointer) {
+            lastIndex = pointer;
+        }
+
+        var size = lastIndex - firstIndex;
+        N[] numb = createNumbersArray(size);
+        int p = 0;
+        for (int i = firstIndex; i < lastIndex; i++) {
+            numb[p] = numbers[i];
+            p++;
+
+        }
+        return numb;
     }
 
-    public MathSet cut(int firstIndex, int lastIndex) {
-        return null;
+    public MathSet<N> cut(int firstIndex, int lastIndex) {
+        if (lastIndex > pointer) {
+            lastIndex = pointer;
+        }
+        if (firstIndex < 0 || firstIndex > lastIndex) {
+            firstIndex = 0;
+        }
+
+        N[] numb = createNumbersArray(numbers.length);
+        int cutCapacity = lastIndex - firstIndex;
+        N[] cutNumb = createNumbersArray(cutCapacity);
+
+        System.arraycopy(this.numbers, firstIndex, cutNumb, 0, cutCapacity);
+
+        System.arraycopy(this.numbers, 0, numb, 0, firstIndex);
+        System.arraycopy(this.numbers, lastIndex, numb, firstIndex, pointer - firstIndex - cutCapacity);
+
+        this.numbers = numb;
+        pointer -= cutCapacity;
+
+        return new MathSet<>(cutNumb);
     }
 
     public void clear() {
-
+        numbers = createNumbersArray(numbers.length);
     }
 
-    public void clear(N[] numbers) {
-
+    @SafeVarargs
+    public final void clear(N... numbers) {
+        for (N number : numbers) {
+            for (int i = 0; i < pointer; i++) {
+                if (Objects.equals(this.numbers[i], number)) {
+                    N[] numb = createNumbersArray(pointer);
+                    System.arraycopy(this.numbers, i + 1, numb, i, pointer - i - 1);
+                    System.arraycopy(this.numbers, 0, numb, 0, i);
+                    this.numbers = numb;
+                    pointer--;
+                    i--;
+                }
+            }
+        }
     }
 
     private void copyToNewArray() {
-        N[] entitiesTmp = numbers;
-        this.numbers = createNumbersArray((numbers.length * 3 / 2) + 1);
-        System.arraycopy(entitiesTmp, 0, this.numbers, 0, pointer);
+        copyToNewArray((numbers.length * 3 / 2) + 1);
+    }
+
+    private void copyToNewArray(int capacity) {
+        if (capacity > numbers.length) {
+            N[] entitiesTmp = numbers;
+            this.numbers = createNumbersArray(capacity);
+            System.arraycopy(entitiesTmp, 0, this.numbers, 0, pointer);
+        }
     }
 
     private N[] createNumbersArray(int capacity) {
-        return (N[]) new Object[capacity];
+        return (N[]) new Number[capacity];
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < pointer; i++) {
+            stringBuilder.append(numbers[i]).append(", ");
+        }
+        return stringBuilder.toString();
     }
 }
