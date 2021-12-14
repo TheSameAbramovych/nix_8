46bd17811481e35c9fe26e3889e0927255de9fcb
TheSameAbramovych <92108412+TheSameAbramovych@users.noreply.github.com>
2021-12-15 00:54:31 +0200

    Delete demo_web directory

--- demo_web/src/main/java/ua/com/alevel/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ua.com.alevel.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ua.com.alevel.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-    boolean existsByEmail(String email);
-}
--- demo_web/src/main/java/ua/com/alevel/service/UserService.java
+++
@@ -1,6 +0,0 @@
-package ua.com.alevel.service;
-
-import ua.com.alevel.entity.User;
-
-public interface UserService extends BaseService<User> {
-}
