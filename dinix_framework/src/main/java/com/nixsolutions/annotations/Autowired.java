5e22934160a526d495f93794dcc26b77ed11a77e
TheSameAbramovych <92108412+TheSameAbramovych@users.noreply.github.com>
2021-12-15 00:54:13 +0200

    Delete dinix_framework directory

--- dinix_framework/src/main/java/com/nixsolutions/annotations/Autowired.java
+++
@@ -1,8 +0,0 @@
-package com.nixsolutions.annotations;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Autowired {
-}
